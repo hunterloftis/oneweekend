c93617259a2387311efd37fd9384af252dc03a6c
Hunter Loftis <hunter@hunterloftis.com>
2019-01-20 15:22:40 -0500

    chapter 4, step 2

--- pkg/tex/perlin.go
+++ pkg/tex/perlin.go
@@ -1,6 +1,7 @@
 package tex
 
 import (
+	"math"
 	"math/rand"
 
 	"github.com/hunterloftis/oneweekend/pkg/geom"
@@ -21,13 +22,24 @@ func init() {
 }
 
 func Perlin(p geom.Vec) float64 {
-	// u := p.X() - math.Floor(p.X())
-	// v := p.Y() - math.Floor(p.Y())
-	// w := p.Z() - math.Floor(p.Z())
-	i := int(4*p.X()) & 255
-	j := int(4*p.Y()) & 255
-	k := int(4*p.Z()) & 255
-	return rndFloat[permX[i]^permY[j]^permZ[k]]
+	u := p.X() - math.Floor(p.X())
+	v := p.Y() - math.Floor(p.Y())
+	w := p.Z() - math.Floor(p.Z())
+	i := int(math.Floor(p.X()))
+	j := int(math.Floor(p.Y()))
+	k := int(math.Floor(p.Z()))
+	c := make([]float64, 8)
+	for di := 0; di < 2; di++ {
+		for dj := 0; dj < 2; dj++ {
+			for dk := 0; dk < 2; dk++ {
+				x := permX[(i+di)&255]
+				y := permY[(j+dj)&255]
+				z := permZ[(k+dk)&255]
+				c[4*di+2*dj+dk] = rndFloat[x^y^z]
+			}
+		}
+	}
+	return trilinear(c, u, v, w)
 }
 
 func perlinGen() []float64 {
@@ -52,4 +64,19 @@ func perlinGenPerm() []int {
 	}
 	perlinPermute(p, 256)
 	return p
+}
+
+func trilinear(c []float64, u, v, w float64) (sum float64) {
+	for i := 0.0; i < 2; i++ {
+		for j := 0.0; j < 2; j++ {
+			for k := 0.0; k < 2; k++ {
+				xyz := c[4*int(i)+2*int(j)+int(k)]
+				sum += (i*u + (1-i)*(1-u)) *
+					(j*v + (1-j)*(1-v)) *
+					(k*w + (1-k)*(1-w)) *
+					xyz
+			}
+		}
+	}
+	return sum
 }
